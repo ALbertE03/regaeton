28d9284b8f659f9c59c827316a993f8e3c78399b
ALbertEO3 <marichalalberto292@gmail.com>
2024-05-26 12:16:57 -0400

    DON

--- data/string.py
+++ data/string.py
@@ -1,55 +1,98 @@
-a = """"Zúmbale mambo, pa' que mis gatas prendan los motore'",
-      "Zúmbale mambo, pa' que mis gatas prendan los motore'", 
-      "Zúmbale mambo, pa' que mis gatas prendan los motore'",
-      "Que se preparen, que lo que viene es pa' que le den (¡duro!)",
-      "Mamita, yo sé que tú no te me vas a quitar (¡duro!)",
-      "Lo que me gusta es que tú te dejas llevar (¡duro!)",
-      "Todos los weekend, ella sale a vacilar (¡duro!)",
-      "Mi gata no para de janguear, porque",
-      "A ella le gusta la gasolina (¡Dame más gasolina!)",
-      "Cómo le encanta la gasolina (¡Dame más gasolina!)",
-      "A ella le gusta la gasolina (¡Dame más gasolina!)", 
-      "Cómo le encanta la gasolina (¡Dame más gasolina!)",
-      "Ella prende las turbinas, no discrimina",
-      "No se pierde ni un party de marquesina",
-      "Se acicala hasta pa' la esquina",
-      "Luce tan bien que hasta la sombra le combina",
-      "Asesina, me domina",
-      "Janguea en carros, motoras y limosinas",
-      "Llena su tanque de adrenalina", 
-      "Cuando escucha reguetón en las bocinas",
-      "A ella le gusta la gasolina (¡Dame más gasolina!)",
-      "Cómo le encanta la gasolina (¡Dame más gasolina!)",
-      "A ella le gusta la gasolina (¡Dame más gasolina!)",
-      "Cómo le encanta la gasolina (¡Dame más gasolina!) yo'",
-      "Aquí somos los mejores, no te me ajore'",
-      "En la pista, nos llaman Los Matadore'",
-      "Tú haces que cualquiera se enamore", 
-      "Cuando bailas al ritmo de los tambore'",
-      "Esto va pa' las gatas de to' colore'",
-      "Pa' las mayore', pa' las menore'",
-      "Pa' las que son más zorras que los cazadore'",
-      "Pa' las mujeres que no apagan sus motore'", 
-      "Tenemos tú y yo algo pendiente",
-      "Tú me debe' algo y lo sabe'",
-      "Conmigo ella se pierde",
-      "No le rinde cuentas a nadie",
-      "Tenemos tú y yo algo pendiente", 
-      "Tú me debe' algo y lo sabe'",
-      "Conmigo ella se pierde",
-      "No le rinde cuentas a nadie",
-      "Zúmbale mambo, pa' que mis gatas prendan los motore'",
-      "Zúmbale mambo, pa' que mis gatas prendan los motore'",
-      "Zúmbale mambo, pa' que mis gatas prendan los motore'",
-      "Que se preparen, que lo que viene es pa' que le den (¡duro!)",
-      "Mamita, yo sé que tú no te me vas a quitar (¡duro!)",
-      "Lo que me gusta es que tú te dejas llevar (¡duro!)",
-      "Todos los weekend, ella sale a vacilar (¡duro!)",
-      "Mi gata no para de janguear, porque", 
-      "A ella le gusta la gasolina (¡Dame más gasolina!)",
-      "Cómo le encanta la gasolina (¡Dame más gasolina!)",
-      "A ella le gusta la gasolina (¡Dame más gasolina!)",
-      "Cómo le encanta la gasolina (¡Dame más gasolina!)"""
+a = """Platinum Studio (respect)
+Don, Don, Don, Don
+
+Salió el Sol (Don, Don)
+Cuerpo bronceado y sus amigas buscaban acción (all the sexy)
+La canción (all my sexy ladies)
+Que causa en ellas y sus cuerpos esa sensación (more reggaeton)
+Reggaeton
+Piden reggaeton
+Del de Don (pon di river, pon di river, pon di river, pon di river)
+Sientan la presión (pon di bank, pon di bank, pon di bank, pon di bank)
+
+Ella baila hasta sola
+Como grande mueve la cola
+La da en pique y la vuela con rola
+Dale y sin miedo rompe la consola
+La pistola, chambonea (reload)
+Ella lo vuelve loco como se menea
+Bailando pura candela
+Lo vuelve loco cuando se acelera
+Y pela
+
+Y yo quiero saber cómo es que baila la Julieta
+(Y yo quiero saber cómo es que baila la Julieta)
+Julieta baila sexy con la mano en la cabeza (my sexy Julieta)
+(Julieta baila sexy con la mano en la cabeza)
+
+Salió el Sol (Don, Don)
+Cuerpo bronceado y sus amigas buscaban acción (aquella canción)
+La canción
+Que causa en ellas y sus cuerpos esa sensación (that's it, need more reggaeton)
+Reggaeton
+Piden reggaeton
+Del de Don (pon di river, pon di river, pon di river, pon di river)
+Sientan la presión (pon di bank, pon di bank, pon di bank, pon di bank)
+
+Va en su viaje (déjala)
+No amarra fuego (pélala)
+Se soltó el pelo (mándala)
+Se rompió el traje (contrólala)
+Lo de ella es playa y arena (yes)
+Se excita cuando el Sol la quema
+Quiere reguetón, discoteca pa' la nena (more fire)
+Se le mete en las venas cuando el negro suena
+
+Y yo quiero saber cómo es que baila la Julieta
+(Y yo quiero saber cómo es que baila la Julieta)
+Julieta baila sexy con la mano en la cabeza
+(Julieta baila sexy con la mano en la cabeza)
+
+Salió el Sol (salió el Sol)
+Cuerpo bronceado y sus amigas buscaban acción (all my sexy)
+La canción
+Que causa en ellas y sus cuerpos esa sensación (more, more, more reggaeton)
+Reggaeton
+Piden reggaeton
+Del de Don (pon di river, pon di river, pon di river, pon di river)
+Sientan la presión (pon di bank, pon di bank, pon di bank, pon di bank)
+
+Respect to all the Jamaica crew
+Señores, han sido testigos de cómo la evolución los ha hecho víctimas de la misma
+Platinum Studio, Julian
+
+Y Don, woah
+Don, woah
+Don, woah
+Ja, tú sabes
+King of Kings
+
+Salió el Sol
+Cuerpo bronceado y sus amigas buscaban acción
+La canción (don't even think about it, papi)
+Que causa en ellas y sus cuerpos esa sensación (you'll never be like this, never)
+Reggaeton (more fire, more fire, more fire, more fire)
+Piden reggaeton (more fire, more fire, more fire, more fire)
+Del de Don (pon di river, pon di river, pon di river, pon di river)
+Sientan la presión (pon di bank, pon di bank, pon di bank, pon di bank)
+
+Whine it
+Whine it, whine it (salió el Sol)
+Whine it, whine it
+Whine it, whine it
+(Pon di river) whine it, whine it
+(Pon di bank) whine it, whine it
+(Pon di bank) whine it, whine it
+
+Salió el Sol (pon di river, pon di river)
+Salió el Sol (pon di bank, pon di bank)
+Salió el Sol (pon di river, pon di river)
+Salió el Sol (pon di bank, pon di bank)
+
+Un número uno, papi
+No hay discusión
+Nosotro' no fallamo'
+King of Kings"""
 a = (
     a.replace(",", "")
     .replace("'", "")
--- script.js
+++ script.js
@@ -83,15 +83,15 @@ function generateGraph(ctx, data) {
         }
     });
 }
-function graph1(ctx1, words, counts) {
+function graph1(ctx1, wordsD, countsD) {
     eliminarGraph()
     return char = new Chart(ctx1, {
         type: 'bar',
         data: {
-            labels: words,
+            labels: wordsD,
             datasets: [{
-                label: 'Frecuencia de palabras',
-                data: counts,
+                label: 'Frecuencia de palabras (Antes)',
+                data: countsD,
                 backgroundColor: 'rgba(54, 162, 235, 0.5)',
                 borderColor: 'rgba(54, 162, 235, 1)',
                 borderWidth: 1
@@ -159,38 +159,52 @@ fetch('data/data.json')
     .then(data => {
         const song = []
         for (const dict of Object.values(data)) {
+
             for (const lyrics of dict) {
                 if ("letra" in lyrics) {
                     song.push(lyrics["letra"])
                 }
             }
         }
-        const letras = song.join(" ")
 
-        const stopwords = ["vamos", 'toma', 'dale', 'dame', 'alaba', 'dónde', 'habríais', 'estéis', 'fueron', 'tuvieses', 'yo', 'tendréis', 'vuestro', 'tuviésemos', 'algo', 'una', 'fuiste', 'tendré', 'tu', 'hayamos', 'estábamos', 'sentido', 'nada', 'sentidos', 'antes', 'ya', 'como', 'siente', 'mi', 'hubiera', 'habrías', 'habíamos', 'fuesen', 'fui', 'los', 'tuviéramos', 'tengas', 'estos', 'tendrías', 'tanto', 'estuvieras', 'tened', 'habiendo', 'fuerais', 'te', 'fueras', 'estuviéramos', 'o', 'al', 'habían', 'fuimos', 'habido', 'tuvieras', 'tenidas', 'habrían', 'su', 'estaban', 'teniendo', 'tus', 'del', 'tuviste', 'que', 'estáis', 'unos', 'estuvieses', 'habéis', 'estaré', 'serías', 'estaríamos', 'tenemos', 'estuvieran', 'estados', 'mía', 'fuisteis', 'porque', 'nosotros', 'esta', 'cual', 'tuvieran', 'algunas', 'estarán', 'vuestra', 'nos', 'seas', 'tengamos', 'tenida', 'erais', 'serían', 'sería', 'estuviste', 'tuve', 'mías', 'de', 'eras', 'estaríais', 'ese', 'han', 'habrán', 'habidos', 'estuvieron', 'eres', 'muchos', 'cuando', 'tenían', 'ellos', 'estando', 'tenía', 'tú', 'estuvieseis', 'tienen', 'con', 'el', 'nuestros', 'esos', 'tendrás', 'ellas', 'tenidos', 'habidas', 'esté', 'fueses', 'sobre', 'pero', 'hay', 'estaréis', 'suyos', 'seáis', 'tienes', 'hubo', 'durante', 'un', 'algunos', 'estuvimos', 'tuvieseis', 'tuvimos', 'seremos', 'ha', 'estarás', 'estará', 'otro', 'más', 'estuvo', 'qué', 'todos', 'habremos', 'estabas', 'somos', 'habida', 'estuvierais', 'era', 'sentida', 'sea', 'tengo', 'le', 'ti', 'eran', 'donde', 'él', 'tuvieron', 'tuviera', 'todo', 'serás', 'fue', 'hubieses', 'teníamos', 'e', 'la', 'has', 'estén', 'estaría', 'vuestros', 'seré', 'muy', 'habías', 'nuestra', 'para', 'tenías', 'hubiéramos', 'tendríais', 'estar', 'tuvo', 'por', 'he', 'teníais', 'estemos', 'fueseis', 'uno', 'fueran', 'sintiendo', 'estuviesen', 'fuéramos', 'hubiste', 'tuviese', 'sin', 'hubierais', 'tendríamos', 'tuyo', 'habréis', 'será', 'tenéis', 'sean', 'otra', 'les', 'nuestro', 'soy', 'contra', 'las', 'también', 'mío', 'estado', 'hayan', 'son', 'éramos', 'esa', 'están', 'ante', 'y', 'tengan', 'vuestras', 'poco', 'estadas', 'tuviesen', 'estás', 'esas', 'nosotras', 'hubieran', 'no', 'desde', 'sus', 'haya', 'hubiese', 'ni', 'os', 'estad', 'tuvisteis', 'este', 'sí', 'mí', 'estamos', 'estuvisteis', 'estuve', 'vosotras', 'mis', 'habrá', 'serán', 'hubieseis', 'fuera', 'se', 'hubieras', 'tendrían', 'estarían', 'hayas', 'a', 'fuésemos', 'sois', 'habré', 'sentid', 'hemos', 'tendremos', 'tendrán', 'nuestras', 'habrás', 'vosotros', 'estaremos', 'hubiesen', 'seamos', 'míos', 'seréis', 'lo', 'suyas', 'estés', 'tenga', 'eso', 'tengáis', 'está', 'habríamos', 'tuyas', 'esto', 'quienes', 'estarías', 'tuyos', 'otras', 'quien', 'entre', 'estuviese', 'estoy', 'estaba', 'estabais', 'es', 'tendrá', 'seríamos', 'hube', 'mucho', 'tenido', 'había', 'tiene', 'sentidas', 'tuvierais', 'hasta', 'estuviera', 'suya', 'estada', 'tendría', 'hubisteis', 'suyo', 'estuviésemos', 'habría', 'en', 'hayáis', 'hubimos', 'habíais', 'hubiésemos', 'tuya', 'otros', 'seríais', 'hubieron', 'fuese', 'estas', 'ella', 'me'];
-        function tokenizeAndFilter(text) {
-            return text.toLowerCase()
-                .replace(/[^a-záéíóúñ\s]/g, '') // Eliminar caracteres no alfabéticos
-                .split(/\s+/)
-                .filter(word => word.length > 3 && !stopwords.includes(word)); // Filtrar stopwords y palabras cortas
+        const song1 = []
+        for (const dict of Object.values(data)) {
+            for (const lyrics of dict) {
+                if ("letra1" in lyrics) {
+                    song.push(lyrics["letra1"])
+                }
+            }
         }
-        // Contar la frecuencia de cada palabra
-        var wordFrequency = tokenizeAndFilter(letras).reduce((freq, word) => {
-            freq[word] = (freq[word] || 0) + 1;
-            return freq
-        }, {});
-        // Ordenar las palabras por frecuencia
-        const sortedWords = Object.entries(wordFrequency)
-            .sort((a, b) => b[1] - a[1])
-            .map(([word, count]) => ({ word, count }));
-        // las 10 palabras más repetidas
-        const topWords = sortedWords.slice(0, 20);
-        const words = topWords.map(item => item.word);
-        const counts = topWords.map(item => item.count);
-        graph1(ctx1, words, counts)
+        const letras0 = song.join(" ")
+        const letras1 = song1.join(" ")
+        const letras = letras0 + letras1
+        util(letras)
+
+        function util(text) {
+            const stopwords = ["vamos", 'toma', 'dale', 'dame', 'alaba', 'dónde', 'habríais', 'estéis', 'fueron', 'tuvieses', 'yo', 'tendréis', 'vuestro', 'tuviésemos', 'algo', 'una', 'fuiste', 'tendré', 'tu', 'hayamos', 'estábamos', 'sentido', 'nada', 'sentidos', 'antes', 'ya', 'como', 'siente', 'mi', 'hubiera', 'habrías', 'habíamos', 'fuesen', 'fui', 'los', 'tuviéramos', 'tengas', 'estos', 'tendrías', 'tanto', 'estuvieras', 'tened', 'habiendo', 'fuerais', 'te', 'fueras', 'estuviéramos', 'o', 'al', 'habían', 'fuimos', 'habido', 'tuvieras', 'tenidas', 'habrían', 'su', 'estaban', 'teniendo', 'tus', 'del', 'tuviste', 'que', 'estáis', 'unos', 'estuvieses', 'habéis', 'estaré', 'serías', 'estaríamos', 'tenemos', 'estuvieran', 'estados', 'mía', 'fuisteis', 'porque', 'nosotros', 'esta', 'cual', 'tuvieran', 'algunas', 'estarán', 'vuestra', 'nos', 'seas', 'tengamos', 'tenida', 'erais', 'serían', 'sería', 'estuviste', 'tuve', 'mías', 'de', 'eras', 'estaríais', 'ese', 'han', 'habrán', 'habidos', 'estuvieron', 'eres', 'muchos', 'cuando', 'tenían', 'ellos', 'estando', 'tenía', 'tú', 'estuvieseis', 'tienen', 'con', 'el', 'nuestros', 'esos', 'tendrás', 'ellas', 'tenidos', 'habidas', 'esté', 'fueses', 'sobre', 'pero', 'hay', 'estaréis', 'suyos', 'seáis', 'tienes', 'hubo', 'durante', 'un', 'algunos', 'estuvimos', 'tuvieseis', 'tuvimos', 'seremos', 'ha', 'estarás', 'estará', 'otro', 'más', 'estuvo', 'qué', 'todos', 'habremos', 'estabas', 'somos', 'habida', 'estuvierais', 'era', 'sentida', 'sea', 'tengo', 'le', 'ti', 'eran', 'donde', 'él', 'tuvieron', 'tuviera', 'todo', 'serás', 'fue', 'hubieses', 'teníamos', 'e', 'la', 'has', 'estén', 'estaría', 'vuestros', 'seré', 'muy', 'habías', 'nuestra', 'para', 'tenías', 'hubiéramos', 'tendríais', 'estar', 'tuvo', 'por', 'he', 'teníais', 'estemos', 'fueseis', 'uno', 'fueran', 'sintiendo', 'estuviesen', 'fuéramos', 'hubiste', 'tuviese', 'sin', 'hubierais', 'tendríamos', 'tuyo', 'habréis', 'será', 'tenéis', 'sean', 'otra', 'les', 'nuestro', 'soy', 'contra', 'las', 'también', 'mío', 'estado', 'hayan', 'son', 'éramos', 'esa', 'están', 'ante', 'y', 'tengan', 'vuestras', 'poco', 'estadas', 'tuviesen', 'estás', 'esas', 'nosotras', 'hubieran', 'no', 'desde', 'sus', 'haya', 'hubiese', 'ni', 'os', 'estad', 'tuvisteis', 'este', 'sí', 'mí', 'estamos', 'estuvisteis', 'estuve', 'vosotras', 'mis', 'habrá', 'serán', 'hubieseis', 'fuera', 'se', 'hubieras', 'tendrían', 'estarían', 'hayas', 'a', 'fuésemos', 'sois', 'habré', 'sentid', 'hemos', 'tendremos', 'tendrán', 'nuestras', 'habrás', 'vosotros', 'estaremos', 'hubiesen', 'seamos', 'míos', 'seréis', 'lo', 'suyas', 'estés', 'tenga', 'eso', 'tengáis', 'está', 'habríamos', 'tuyas', 'esto', 'quienes', 'estarías', 'tuyos', 'otras', 'quien', 'entre', 'estuviese', 'estoy', 'estaba', 'estabais', 'es', 'tendrá', 'seríamos', 'hube', 'mucho', 'tenido', 'había', 'tiene', 'sentidas', 'tuvierais', 'hasta', 'estuviera', 'suya', 'estada', 'tendría', 'hubisteis', 'suyo', 'estuviésemos', 'habría', 'en', 'hayáis', 'hubimos', 'habíais', 'hubiésemos', 'tuya', 'otros', 'seríais', 'hubieron', 'fuese', 'estas', 'ella', 'me'];
+            function tokenizeAndFilter(text) {
+                return text.toLowerCase()
+                    .replace(/[^a-záéíóúñ\s]/g, '') // Eliminar caracteres no alfabéticos
+                    .split(/\s+/)
+                    .filter(word => word.length > 3 && !stopwords.includes(word)); // Filtrar stopwords y palabras cortas
+            }
+            // Contar la frecuencia de cada palabra
+            var wordFrequency = tokenizeAndFilter(text).reduce((freq, word) => {
+                freq[word] = (freq[word] || 0) + 1;
+                return freq
+            }, {});
+            // Ordenar las palabras por frecuencia
+            const sortedWords = Object.entries(wordFrequency)
+                .sort((a, b) => b[1] - a[1])
+                .map(([word, count]) => ({ word, count }));
+            // las 10 palabras más repetidas
+            const topWords = sortedWords.slice(0, 20);
+            const words = topWords.map(item => item.word);
+            const counts = topWords.map(item => item.count);
+            graph1(ctx1, words, counts)
+        }
 
     })
-
     .catch(error => {
         console.error("error al cargar el Json " + error)
     });
