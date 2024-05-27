22d740efceb6d058e8b6976fed0a7af0f6231e71
ALbertEO3 <marichalalberto292@gmail.com>
2024-05-27 10:59:43 -0400

    .

--- script.js
+++ script.js
@@ -5,6 +5,7 @@ const modalInfo = document.getElementById("modalInfo");
 let graphCanvas = document.getElementById('graph')
 let ctx = graphCanvas.getContext('2d');
 const ctx1 = document.getElementById('wordFrequencyChart').getContext('2d');
+const ctx2 = document.getElementById('actual').getContext('2d');
 let char;
 document.addEventListener('click', function (event) {
     let dropdownContent = document.getElementById('dropdown-content');
@@ -95,6 +96,39 @@ function generateGraph(ctx, data) {
         }
     });
 }
+function graph2(ctx1, words, counts) {
+    return char = new Chart(ctx1, {
+        type: 'bar',
+        data: {
+            labels: words,
+            datasets: [{
+                label: 'Frecuencia de palabras (Actual)',
+                data: counts,
+                backgroundColor: 'rgba(54, 162, 235, 0.5)',
+                borderColor: 'white',
+                pointBackgroundColor: "white",
+                borderWidth: 1,
+                pointBorderColor: "white",
+            }]
+        },
+        options: {
+            maintainAspectRadio: false,
+            responsive: true,
+            scales: {
+                y: {
+                    ticks: { color: "white" },
+                    beginAtZero: true
+                }
+            }, x: {
+                ticks: {
+                    color: "white"
+                },
+            }
+        }
+    });
+}
+
+
 function graph1(ctx1, words, counts) {
     eliminarGraph()
     return char = new Chart(ctx1, {
@@ -173,7 +207,7 @@ let dataDonOmar = {
 };
 
 
-//Json
+//Json1
 fetch('data/data.json')
     .then(response => response.json())
     .then(data => {
@@ -230,6 +264,72 @@ fetch('data/data.json')
             const words = topWords.map(item => item.word);
             const counts = topWords.map(item => item.count);
             graph1(ctx1, words, counts)
+        }
+
+    })
+    .catch(error => {
+        console.error("error al cargar el Json " + error)
+    });
+
+
+
+//json2
+fetch('data/data1.json')
+    .then(response => response.json())
+    .then(data => {
+        const song_actual = []
+        for (const dict of Object.values(data)) {
+            for (const lyrics of dict) {
+                if ("letra" in lyrics) {
+                    song_actual.push(lyrics["letra"])
+                }
+            }
+        }
+
+        const song_actual1 = []
+        for (const dict1 of Object.values(data)) {
+            for (const lyrics1 of dict1) {
+                if ("letra1" in lyrics1) {
+                    song_actual1.push(lyrics1["letra1"])
+                }
+            }
+        }
+        const song_actual2 = []
+        for (const dict2 of Object.values(data)) {
+            for (const lyrics2 of dict2) {
+                if ("letra2" in lyrics2) {
+                    song_actual2.push(lyrics2["letra2"])
+                }
+            }
+        }
+        const letras_actual0 = song_actual.join(" ")
+        const letras_actual1 = song_actual1.join(" ")
+        const letras_actual2 = song_actual2.join(" ")
+        const letras_actual = (letras_actual0 + letras_actual1) + letras_actual2
+        util1(letras_actual)
+
+        function util1(text) {
+            const stopwords = ["vamos", 'toma', 'dale', 'dame', 'alaba', 'dónde', 'habríais', 'estéis', 'fueron', 'tuvieses', 'yo', 'tendréis', 'vuestro', 'tuviésemos', 'algo', 'una', 'fuiste', 'tendré', 'tu', 'hayamos', 'estábamos', 'sentido', 'nada', 'sentidos', 'antes', 'ya', 'como', 'siente', 'mi', 'hubiera', 'habrías', 'habíamos', 'fuesen', 'fui', 'los', 'tuviéramos', 'tengas', 'estos', 'tendrías', 'tanto', 'estuvieras', 'tened', 'habiendo', 'fuerais', 'te', 'fueras', 'estuviéramos', 'o', 'al', 'habían', 'fuimos', 'habido', 'tuvieras', 'tenidas', 'habrían', 'su', 'estaban', 'teniendo', 'tus', 'del', 'tuviste', 'que', 'estáis', 'unos', 'estuvieses', 'habéis', 'estaré', 'serías', 'estaríamos', 'tenemos', 'estuvieran', 'estados', 'mía', 'fuisteis', 'porque', 'nosotros', 'esta', 'cual', 'tuvieran', 'algunas', 'estarán', 'vuestra', 'nos', 'seas', 'tengamos', 'tenida', 'erais', 'serían', 'sería', 'estuviste', 'tuve', 'mías', 'de', 'eras', 'estaríais', 'ese', 'han', 'habrán', 'habidos', 'estuvieron', 'eres', 'muchos', 'cuando', 'tenían', 'ellos', 'estando', 'tenía', 'tú', 'estuvieseis', 'tienen', 'con', 'el', 'nuestros', 'esos', 'tendrás', 'ellas', 'tenidos', 'habidas', 'esté', 'fueses', 'sobre', 'pero', 'hay', 'estaréis', 'suyos', 'seáis', 'tienes', 'hubo', 'durante', 'un', 'algunos', 'estuvimos', 'tuvieseis', 'tuvimos', 'seremos', 'ha', 'estarás', 'estará', 'otro', 'más', 'estuvo', 'qué', 'todos', 'habremos', 'estabas', 'somos', 'habida', 'estuvierais', 'era', 'sentida', 'sea', 'tengo', 'le', 'ti', 'eran', 'donde', 'él', 'tuvieron', 'tuviera', 'todo', 'serás', 'fue', 'hubieses', 'teníamos', 'e', 'la', 'has', 'estén', 'estaría', 'vuestros', 'seré', 'muy', 'habías', 'nuestra', 'para', 'tenías', 'hubiéramos', 'tendríais', 'estar', 'tuvo', 'por', 'he', 'teníais', 'estemos', 'fueseis', 'uno', 'fueran', 'sintiendo', 'estuviesen', 'fuéramos', 'hubiste', 'tuviese', 'sin', 'hubierais', 'tendríamos', 'tuyo', 'habréis', 'será', 'tenéis', 'sean', 'otra', 'les', 'nuestro', 'soy', 'contra', 'las', 'también', 'mío', 'estado', 'hayan', 'son', 'éramos', 'esa', 'están', 'ante', 'y', 'tengan', 'vuestras', 'poco', 'estadas', 'tuviesen', 'estás', 'esas', 'nosotras', 'hubieran', 'no', 'desde', 'sus', 'haya', 'hubiese', 'ni', 'os', 'estad', 'tuvisteis', 'este', 'sí', 'mí', 'estamos', 'estuvisteis', 'estuve', 'vosotras', 'mis', 'habrá', 'serán', 'hubieseis', 'fuera', 'se', 'hubieras', 'tendrían', 'estarían', 'hayas', 'a', 'fuésemos', 'sois', 'habré', 'sentid', 'hemos', 'tendremos', 'tendrán', 'nuestras', 'habrás', 'vosotros', 'estaremos', 'hubiesen', 'seamos', 'míos', 'seréis', 'lo', 'suyas', 'estés', 'tenga', 'eso', 'tengáis', 'está', 'habríamos', 'tuyas', 'esto', 'quienes', 'estarías', 'tuyos', 'otras', 'quien', 'entre', 'estuviese', 'estoy', 'estaba', 'estabais', 'es', 'tendrá', 'seríamos', 'hube', 'mucho', 'tenido', 'había', 'tiene', 'sentidas', 'tuvierais', 'hasta', 'estuviera', 'suya', 'estada', 'tendría', 'hubisteis', 'suyo', 'estuviésemos', 'habría', 'en', 'hayáis', 'hubimos', 'habíais', 'hubiésemos', 'tuya', 'otros', 'seríais', 'hubieron', 'fuese', 'estas', 'ella', 'me'];
+            function tokenizeAndFilter(text) {
+                return text.toLowerCase()
+                    .replace(/[^a-záéíóúñ\s]/g, '') // Eliminar caracteres no alfabéticos
+                    .split(/\s+/)
+                    .filter(word => word.length > 3 && !stopwords.includes(word)); // Filtrar stopwords y palabras cortas
+            }
+            // Contar la frecuencia de cada palabra
+            let wordFrequency = tokenizeAndFilter(text).reduce((freq, word) => {
+                freq[word] = (freq[word] || 0) + 1;
+                return freq
+            }, {});
+            // Ordenar las palabras por frecuencia
+            const sortedWords = Object.entries(wordFrequency)
+                .sort((a, b) => b[1] - a[1])
+                .map(([word, count]) => ({ word, count }));
+            // las 15 palabras más repetidas
+            const topWords = sortedWords.slice(0, 15);
+            const words = topWords.map(item => item.word);
+            const counts = topWords.map(item => item.count);
+            graph2(ctx2, words, counts)
         }
 
     })
